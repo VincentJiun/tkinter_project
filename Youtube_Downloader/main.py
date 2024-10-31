ffff23b37c6fff13d30e3bb166615f88a214cbd5
vincent7955168@gmail.com <ljjain0508@gmail.com>
2024-10-31 23:41:12 +0800

    youtube downloder entries

--- Youtube_Downloader/main.py
+++ Youtube_Downloader/main.py
@@ -11,7 +11,7 @@ class App(ctk.CTk):
 
     def index(self):
         self.index_page = IndexPage(self)
-        self.index_page.pack(fill='both', expand=True, padx=20, pady=20)
+        self.index_page.pack(fill='both', expand=True)
         self.index_page.create_page()
 
 class IndexPage(ctk.CTkFrame):
@@ -19,12 +19,29 @@ class IndexPage(ctk.CTkFrame):
         super().__init__(master)
 
     def create_page(self):
-        # page frame
-        self.frame_container = ctk.CTkFrame(self)
+        # Frame Container
+        self.frame_container = ctk.CTkFrame(self, fg_color='#B8B8DC', corner_radius=0, border_color='#B8B8DC')
         self.frame_container.pack(fill='both', expand=True)
         # Widgets
-        self.test_label = ctk.CTkLabel(self.frame_container, text='text', font=('times new roman', 30, 'bold'))
-        self.test_label.place(relx=0.5, rely=0.5, anchor='center')
+        # Title Label
+        self.label_title = ctk.CTkLabel(self.frame_container, text='Youtube Downloader', font=('Times', 20, 'bold'))
+        self.label_title.place(relx=0.5, rely=0.05, relwidth=1, relheight=0.1, anchor='center')
+        # URL Entry
+        self.label_url = ctk.CTkLabel(self.frame_container, text='URL:', font=('Times', 14, 'bold'))
+        self.label_url.place(relx=0.1, rely=0.15, relwidth=0.2, relheight=0.06, anchor='center')
+        self.entry_url = ctk.CTkEntry(self.frame_container, font=('Times', 14, 'bold'))
+        self.entry_url.place(relx=0.4, rely=0.15, relwidth=0.55, relheight=0.06, anchor='center')
+        self.button_mp3 = ctk.CTkButton(self.frame_container, text='(.mp3)', font=('Times', 14, 'bold'), command=self.download_mp3)
+        self.button_mp3.place(relx=0.75, rely=0.15, relwidth=0.08, relheight=0.06, anchor='center')
+        self.button_mp4 = ctk.CTkButton(self.frame_container, text='(.mp4)', font=('Times', 14, 'bold'), command=self.download_mp4)
+        self.button_mp4.place(relx=0.85, rely=0.15, relwidth=0.08, relheight=0.06, anchor='center')
+
+    def download_mp3(self):
+        self.url = self.entry_url.get()
+        print(self.url)
+
+    def download_mp4(self):
+        pass
 
 
 
